5f7ba46c77d0fcda9103b8d4882dffaa487e124e
yulai <yulai@yahoo-inc.com>
2015-12-09 17:03:26 -0800

    change

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
+
 var mongoose = require('mongoose');
- 
+
 module.exports = mongoose.model('User',{
-        username: String,
-    password: String,
-    email: String,
-    gender: String,
-    address: String
+	id: String,
+	username: String,
+	password: String,
+	email: String,
+	firstName: String,
+	lastName: String
 });
