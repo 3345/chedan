ee65910c62c928e03de419c0d55f459d0cf9bb04
yulai <yulai@yahoo-inc.com>
2015-12-09 17:53:18 -0800

    err msg

--- passport/login.js
+++ passport/login.js
@@ -17,12 +17,13 @@ module.exports = function(passport){
                     // Username does not exist, log the error and redirect back
                     if (!user){
                         console.log('User Not Found with username '+username);
-                        return done(null, false, req.flash('message', 'User Not found.'));                 
+                        return done(null, false, req.flash('message', '用户名不存在，再见'));                 
                     }
+                    
                     // User exists but wrong password, log the error 
                     if (!isValidPassword(user, password)){
                         console.log('Invalid Password');
-                        return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+                        return done(null, false, req.flash('message', '密码不对，再见')); // redirect back to login page
                     }
                     // User and password both match, return user from done method
                     // which will be treated like success
